c48285f5350a5dcda838b8fcf92326f32f0d7d14
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-06-12 15:57:04 +0100

    Customize input controls

--- src/ApplicationCore/Helpers/PixelSize.cs
+++ src/ApplicationCore/Helpers/PixelSize.cs
@@ -2,14 +2,18 @@
 {
     public class PixelSize
     {
+        public PixelSize()
+        {
+        }
+        
         public PixelSize(int width, int height)
         {
             Width = width;
             Height = height;
         }
-        
-        public int Width { get; }
-        
-        public int Height { get; }
+
+        public int Width { get; set; } = 100;
+
+        public int Height { get; set; } = 100;
     }
 }
