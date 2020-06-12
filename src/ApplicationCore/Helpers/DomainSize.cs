0da6287246d536d4da14a3f3887067f16d7abbad
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-06-12 16:13:21 +0100

    Add controls to customize all fractal settings (#24)
    * Start building proper fractal control
    
    * Customize input controls
    
    * Improve result layout

--- src/ApplicationCore/Helpers/DomainSize.cs
+++ src/ApplicationCore/Helpers/DomainSize.cs
@@ -2,6 +2,10 @@
 {
     public class DomainSize
     {
+        public DomainSize()
+        {
+        }
+        
         public DomainSize(double minX, double maxX, double minY, double maxY)
         {
             MinX = minX;
@@ -9,13 +13,13 @@
             MinY = minY;
             MaxY = maxY;
         }
-        
-        public double MinX { get; }
-        
-        public double MaxX { get; }
-        
-        public double MinY { get; }
-        
-        public double MaxY { get; }
+
+        public double MinX { get; set; } = -1;
+
+        public double MaxX { get; set; } = 1;
+
+        public double MinY { get; set; } = -1;
+
+        public double MaxY { get; set; } = 1;
     }
 }
--- src/ApplicationCore/Helpers/FractalOptions.cs
+++ src/ApplicationCore/Helpers/FractalOptions.cs
@@ -1,18 +1,16 @@
-﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.ColorSpaces;
-using SixLabors.ImageSharp.PixelFormats;
+﻿using SixLabors.ImageSharp.ColorSpaces;
 
 namespace ApplicationCore.Helpers
 {
     public class FractalOptions
     {
-        public PixelSize PixelSize { get; set; }
+        public PixelSize PixelSize { get; set; } = new PixelSize();
         
-        public DomainSize DomainSize { get; set; }
-        
-        public int MaxIterations { get; set; }
-        
-        public double Precision { get; set; }
+        public DomainSize DomainSize { get; set; } = new DomainSize();
+
+        public int MaxIterations { get; set; } = 50;
+
+        public double Precision { get; set; } = 1e-5;
         
         public float Depth { get; set; }
         
@@ -20,6 +18,6 @@ namespace ApplicationCore.Helpers
         
         public int Threshold { get; set; }
         
-        public Hsv FillColor { get; set; }
+        public Hsv FillColor { get; set; } = new Hsv(0, 0, 0);
     }
 }
