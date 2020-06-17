7f014899c7a1fdcdd5dee61733a5de4257ab2412
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-06-17 15:13:04 +0100

    Add tests to Azure Function

---
+++ tests/FunctionTests/MainTests.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using ApplicationCore.Helpers;
+using Function;
+using FunctionTests.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace FunctionTests
+{
+    public class MainTests
+    {
+        private readonly ILogger _logger = TestFactory.CreateLogger();
+        
+        [Test]
+        public async Task CanGenerateFractalAsync()
+        {
+            var options = new ExtendedFractalOptions
+            {
+                Expression = "z^3+1",
+            };
+
+            var result = await CallFunction(options);
+            Assert.Null(result.ErrorMessage);
+        }
+
+        [TestCase("z+")]
+        [TestCase("(z")]
+        [TestCase("z)")]
+        [TestCase("sin()")]
+        public async Task CanDetectParseErrors(string expression)
+        {
+            var options = new ExtendedFractalOptions
+            {
+                Expression = expression,
+            };
+
+            var result = await CallFunction(options);
+            Assert.NotNull(result.ErrorMessage);
+        }
+        
+        private async Task<FunctionResult> CallFunction(ExtendedFractalOptions options)
+        {
+            var request = TestFactory.CreateHttpRequest(options);
+            var result = (OkObjectResult) await FractalGenerator.Run(request, _logger);
+            return (FunctionResult) result.Value;;
+        }
+    }
+}
